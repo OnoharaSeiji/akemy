41122804e1e5db9728eb87801052b9612e36ef12
Onohara Seiji <166080965+OnoharaSeiji@users.noreply.github.com>
2026-03-23 21:21:13 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  memberName: "Akemy",
+  memberSlug: "akemy",
+  startDate: "2026-03-23",
+  totalDays: 200,
+  unlockedDays: 200,
+  apostilaPath: "./apostilas",
+  apostilaPrefix: "dia-"
+};
